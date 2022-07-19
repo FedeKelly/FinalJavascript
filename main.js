fcf51283cecd474b11127c275d47c6e56bd6ac03
FedeKelly <kellyfede@hotmail.com>
2022-07-19 19:46:38 -0300

    version objetos arrays high order

--- main.js
+++ main.js
@@ -19,38 +19,101 @@ while(anchoPlancha < anchoMinimoPlanchaCorrugar){
     alert("Ancho de plancha menor al mínimo")
     anchoPlancha = parseInt(prompt("Decime El Ancho de plancha en mm"))
 } 
- 
-console.log("La Plancha definida es " + largoPlancha + " de largo x " + anchoPlancha + " de ancho")
+let abiertaOCerrada = prompt("Caja Abierta o Cerrada?").toUpperCase()
+while(abiertaOCerrada != "ABIERTA" && abiertaOCerrada != "CERRADA"){
+    alert("Tipo de caja no definido")
+    abiertaOCerrada = prompt("Caja Abierta o Cerrada?").toUpperCase()
+}  
+
+console.log("La Plancha definida es " + largoPlancha + " de largo x " + anchoPlancha + " de ancho" + "de tipo " + abiertaOCerrada)
 // Info solicitada al usuario
 
-for(let i = 1; i <= 7 ; i++){
-    let resultado = i * anchoPlancha;
-    console.log(resultado);
-    if((anchoFormato1900 - resultado <= refileMaximoCorrugadora && anchoFormato1900 - resultado>= refileMinimoCorrugadora) || (anchoFormato2100 - resultado <= refileMaximoCorrugadora && anchoFormato2100 - resultado >= refileMinimoCorrugadora) || (anchoFormato2250 - resultado <= refileMaximoCorrugadora && anchoFormato2250 - resultado >= refileMinimoCorrugadora) || (anchoFormato2400 - resultado <= refileMaximoCorrugadora && anchoFormato2400 - resultado >= refileMinimoCorrugadora)){
-        alert("Tu caja autocombina")
-    }else{
-        alert("Tu caja NO autocombina")
-    }
+const multiplicacionAncho = []
+for(let i = 1; (anchoPlancha * i) <= anchoFormato2400 ; i++){
+    multiplicacionAncho.push(anchoPlancha * i)
+    console.log(multiplicacionAncho);
 }
 
-// Fin desafío entregable
+const definicionAutocombinacion = []
 
-// Inicio práctica OBJETOS
+for(let i = 0; i < multiplicacionAncho.length; i++){
+    if((anchoFormato1900 - multiplicacionAncho[i] <= refileMaximoCorrugadora && anchoFormato1900 - multiplicacionAncho[i] >= refileMinimoCorrugadora)){
+        alert("Tu caja autocombina en 1900")
+        definicionAutocombinacion.push(anchoFormato1900)
+    }
+    }
+for(let i = 0; i < multiplicacionAncho.length; i++){
+    if((anchoFormato2100 - multiplicacionAncho[i] <= refileMaximoCorrugadora && anchoFormato2100 - multiplicacionAncho[i] >= refileMinimoCorrugadora)){
+        alert("Tu caja autocombina en 2100")
+        definicionAutocombinacion.push(anchoFormato2100)
+    }
+    }
+for(let i = 0; i < multiplicacionAncho.length; i++){
+    if((anchoFormato2250 - multiplicacionAncho[i] <= refileMaximoCorrugadora && anchoFormato2250 - multiplicacionAncho[i] >= refileMinimoCorrugadora)){
+        alert("Tu caja autocombina en 2250")
+        definicionAutocombinacion.push(anchoFormato2250)
+    }
+    }
+for(let i = 0; i < multiplicacionAncho.length; i++){
+    if((anchoFormato2400 - multiplicacionAncho[i] <= refileMaximoCorrugadora && anchoFormato2400 - multiplicacionAncho[i] >= refileMinimoCorrugadora)){
+        alert("Tu caja autocombina en 2400")
+        definicionAutocombinacion.push(anchoFormato2400)
+    }
+    }
+console.log(definicionAutocombinacion)
+if(definicionAutocombinacion.length == 0){
+    alert("Tu caja NO autocombina")
+}
 
-function maquina(abiertaCerrada, anchoMaximo, anchoMinimo, largoMaximo, LargoMinimo) {
+// Definición de Objetos Máquinas
+
+function maquina(ubicacion, nombreMaquina, abiertaCerrada, anchoMaximo, anchoMinimo, largoMaximo, largoMinimo) {
+    this.ubicacion = ubicacion
+    this.nombreMaquina = nombreMaquina;
     this.abiertaCerrada = abiertaCerrada;
     this.anchoMaximo = anchoMaximo; 
     this.anchoMinimo = anchoMinimo;
     this.largoMaximo = largoMaximo;
-    this.LargoMinimo = LargoMinimo;
+    this.largoMinimo = largoMinimo;
 }
 
-const maquinaWard1 = new maquina("abierta", 1600, 600, 2450, 1200);
-const maquinaNT = new maquina("Cerrada", 1500, 450, 2500, 600)
-const maquinaDRO = new maquina ("abierta", 1400, 400, 2200, 800)
-const maquinaWard3 = new maquina ("Cerrada", 1600, 500, 2650, 700)
+const maquinas = []
+const maquinaWard1 = new maquina("Parana", "Ward1","ABIERTA", 1600, 600, 2450, 1200);
+maquinas.push (maquinaWard1)
+const maquinaNT = new maquina("Parana", "Martin NT", "CERRADA", 1500, 450, 2500, 600)
+maquinas.push(maquinaNT)
+const maquinaDRO = new maquina ("Parana", "DRO", "ABIERTA", 1400, 400, 2200, 800)
+maquinas.push(maquinaDRO)
+const maquinaWard3 = new maquina ("Parana", "Ward3","CERRADA", 1600, 500, 2650, 700)
+maquinas.push(maquinaWard3)
+
 
 console.log(maquinaWard1)
 console.log(maquinaNT)
 console.log(maquinaDRO)
 console.log(maquinaWard3)
+console.log(maquinas)
+
+const filtroAbiertaCerrada = maquinas.filter((el) => el.abiertaCerrada === abiertaOCerrada)
+console.log(filtroAbiertaCerrada)
+
+const filtroAnchoMax = filtroAbiertaCerrada.filter((el) => el.anchoMaximo > anchoPlancha)
+console.log(filtroAnchoMax)
+
+const filtroAnchoMin = filtroAnchoMax.filter((el) => el.anchoMinimo < anchoPlancha)
+console.log(filtroAnchoMin)
+
+const filtroLargoMax = filtroAnchoMin.filter((el) => el.largoMaximo > largoPlancha)
+console.log(filtroLargoMax)
+
+const filtroLargoMin = filtroLargoMax.filter((el) => el.largoMinimo < largoPlancha)
+console.log(filtroLargoMin)
+
+const filtroNombres = filtroLargoMin.map((el) => el.nombreMaquina)
+console.log(filtroNombres)
+
+if(filtroNombres.length === 0){
+    alert ("Tu caja no se puede fabricar")
+} else {
+    alert ("El flujo productivo probable es " + filtroNombres.join())
+}
